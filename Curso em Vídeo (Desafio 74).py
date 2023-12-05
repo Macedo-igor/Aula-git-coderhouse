4141bc05721d1ae0715cd5b030d87a78d9a23b21
Macedo-igor <152926178+Macedo-igor@users.noreply.github.com>
2023-12-05 08:43:34 -0300

    Update Curso em Vídeo (Desafio 74).py

--- Curso em Vídeo (Desafio 74).py
+++ Curso em Vídeo (Desafio 74).py
@@ -1,1 +1,9 @@
+from random import randint
+numero = (randint(1,1000),randint(1,1000),randint(1,1000),randint(1,1000),randint(1,1000))
+print("Os valores sorteados foram:")
+
+for c in numero:
+    print(c, end= ' ')
+print(f"\nO maior valor desta tupla é: {max(numero)}")
+print(f"O menor valor desta tupla é: {min(numero)}")
 
