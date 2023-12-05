66a9365dcfb11ed73981358d2990e224454f2946
Igor Macedo <igorgomesmacedo@outlook.com>
2023-12-04 23:46:30 -0300

    Desafio 79-curso-em-video

---
+++ Curso em Vídeo (Desafio 79).py
@@ -0,0 +1,17 @@
+lista = []
+
+while True:
+    numeros = int(input("Digite um valor: "))
+    
+    if numeros in lista:
+        print("Valor duplicado! Tente novamente!")
+    else:
+        lista.append(numeros)
+        print("Valor adicionado com sucesso!")
+        
+    opção = input("Quer continuar? [S/N]:  ")
+    if opção.upper() == 'N':
+        break
+    
+print('=-' * 30)               
+print(f"Você digitou os números {sorted(lista)}")
